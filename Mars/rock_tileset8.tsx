239859a46f588bd6c83ed1d27ca8c75924ae7d62
Matsvei Haidukou <s26169@pjwstk.edu.pl>
2024-12-07 22:29:02 +0100

    f

---
+++ Mars/rock_tileset8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rock_tileset8" tilewidth="16" tileheight="16" tilecount="49" columns="7">
+ <image source="rock_tileset8.png" width="112" height="112"/>
+</tileset>
